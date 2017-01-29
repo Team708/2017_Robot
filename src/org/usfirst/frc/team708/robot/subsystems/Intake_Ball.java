610a3def1873d2be391656875544bbec27a7c021
mgorkes <mgorkes@aol.com>
2017-01-29 17:52:01 -0500

    Intake_Ball Subsytem

--- src/org/usfirst/frc/team708/robot/subsystems/Intake_Ball.java
+++ src/org/usfirst/frc/team708/robot/subsystems/Intake_Ball.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team708.robot.subsystems;
 
 import org.usfirst.frc.team708.robot.Constants;
+import org.usfirst.frc.team708.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
 //import org.usfirst.frc.team708.robot.RobotMap;
 //import edu.wpi.first.wpilibj.Relay;
 //import edu.wpi.first.wpilibj.Relay.Value;
@@ -9,30 +13,34 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  * Subsystem that intakes balls
- * @author James_Makovics
- * @author Michael_Steinberg
- * @author Thomas Zhao
- * @author Alex Tysak
+ * @author Madison
+ * @author Nick
  */
 
-public class Intake extends Subsystem {
+public class Intake_Ball extends Subsystem {
 	
-    /**
-      * Constructor
-      */
-	public Intake() {
-	
+    private CANTalon intakeMotor;
+
+	//I'm trying to link the right motor to the intake code here
+	public Intake_Ball() {
+		
+		intakeMotor = new CANTalon (RobotMap.intakeMotorBall);
+		
 	}
 	
 	public void initDefaultCommand() {
         
     }
 	
+	//I believe this sets the speed of the motor
 	public void moveMotor(double speed) {
 		
+		intakeMotor.set(speed);
 	}
-	
+	//I believe this stops the motor
 	public void stop(){
+		
+		intakeMotor.set(Constants.INTAKE_OFF)
 		
 	}
     
