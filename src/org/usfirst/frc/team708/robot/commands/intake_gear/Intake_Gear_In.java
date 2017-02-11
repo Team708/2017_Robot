69d6894e0b9bc6d041d8b36951bfc9b3d727986c
jpiergal <john.piergallini@lmco.com>
2017-02-11 00:29:04 -0500

    Intake and Pivot on IR sensor

--- src/org/usfirst/frc/team708/robot/commands/intake_gear/Intake_Gear_In.java
+++ src/org/usfirst/frc/team708/robot/commands/intake_gear/Intake_Gear_In.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team708.robot.commands.intake_gear;
 
 import org.usfirst.frc.team708.robot.Constants;
 import org.usfirst.frc.team708.robot.Robot;
+import org.usfirst.frc.team708.robot.subsystems.Pivot_Gear;
+import org.usfirst.frc.team708.robot.commands.intake_gear.*;
+
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,13 +12,20 @@ public class Intake_Gear_In extends Command {
 
 	public Intake_Gear_In() {	
 		requires(Robot.intake_gear);
+		requires(Robot.pivot_gear);
 	}
 
 	protected void initialize() {
 	}
 
 	protected void execute() {
-		Robot.intake_gear.moveMotor(Constants.INTAKE_FORWARD);
+		if (!Robot.intake_gear.hasGear())
+		    Robot.intake_gear.moveMotor(Constants.INTAKE_FORWARD);
+		else
+		{
+			Robot.intake_gear.stop();
+			Robot.pivot_gear.moveMotor(Constants.INTAKE_FORWARD);
+		}
 	}
 
 	protected boolean isFinished() {
@@ -23,7 +33,8 @@ public class Intake_Gear_In extends Command {
 	}
 
 	protected void end() {
-		Robot.intake_gear.stop();
+			Robot.intake_gear.stop();
+			Robot.pivot_gear.stop();
 	}
 
 	protected void interrupted() {
